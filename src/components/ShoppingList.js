02d42a0e6c910b373b58b3cfd5c1e993e2a90b58
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-06-25 15:49:30 -0600

    solution

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React,{useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  // useState() return [inital, function]
+  const [category, setCategory] = useState("All")
+
+  console.log(category)
+  // function simpleState(initial){
+  //   const variable = initial
+  //   function setVariable(){
+  //     // BE ABLE TO CHANGE STATE
+  //     // BE ABLE TO TELL REACT TO RERENDER
+  //   }
+  //   return [variable,setVariable]
+  // }
+
+  const filteredItems = items.filter((item)=>{
+    if (item.category === category || category === "All"){
+      return true
+    }
+    return false
+  })
+  // console.log(filteredItems)
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={(e)=>setCategory(e.target.value)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +34,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
